bd1faba0381427f9dfdf4a7eb7d93ff3e3e4bfaf
mahesh353 <pritampatil353@gmail.com>
2023-06-24 22:55:50 +0530

    added initial notes for Episode 2

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-var heading = React.createElement("h1", {id:"heading"}, "Hello World from React!");
+var heading = React.createElement("h1", { id: "heading" }, "Hello World from React!");
 var root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
