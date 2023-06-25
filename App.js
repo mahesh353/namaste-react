be3ebb726ec293a968865c7545173a6165967df0
mahesh353 <pritampatil353@gmail.com>
2023-06-25 14:46:37 +0530

    Initial commit of episode 3

--- App.js
+++ App.js
@@ -1,6 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-var heading = React.createElement("h1", { id: "heading" }, "Hello World from React!");
+//React.CreateElement => React elements - JS Object => HtmlElement(When we render it);
+var heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Namaste React123 !"
+);
+
+// JSX => React.CreateElement => React elements - JS Object => HtmlElement(When we render it);
+// React Element
+const jsxHeading = (
+  <h1 id="heading" className="head">
+    Namaste React using JSX !
+  </h1>
+);
+
 var root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+root.render(jsxHeading);
